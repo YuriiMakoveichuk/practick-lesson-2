efe9d947384a3747e7a10fcd693b878e5d02e3ea
Mitka92 <134132348+Mitka92@users.noreply.github.com>
2024-08-04 13:05:55 +0300

    task-1-finish

--- src/components/PhotosGallery/PhotosGallery.jsx
+++ src/components/PhotosGallery/PhotosGallery.jsx
@@ -1,3 +1,17 @@
-export const PhotosGallery = () => {
-  return <h3>PhotosGallery</h3>;
+import { Grid, PhotosGalleryItem } from '..';
+
+export const PhotosGallery = ({ images, openModal }) => {
+  return (
+    <Grid>
+      {images.map(({ id, src, alt, avg_color }) => (
+        <PhotosGalleryItem
+          key={id}
+          src={src}
+          alt={alt}
+          avgColor={avg_color}
+          openModal={openModal}
+        />
+      ))}
+    </Grid>
+  );
 };
